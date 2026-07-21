789d113dd6e7b86e61121b4df20c9db45abc5703
Kwartler <you@example.com>
2026-07-20 22:36:31 -0400

    wip day 1 and 2 work

---
+++ lessons/day1/keep_for_now/Z_openrouter_prompting.R
@@ -0,0 +1,61 @@
+#' Author: Ted Kwartler 
+#' An example for students using OpenRouter.ai cloud API for model calls.
+#' July 14 2026
+
+library(httr2)
+OPENROUTER_API_KEY <- Sys.getenv("OPENROUTER_API_KEY")
+
+# ---- Edit these ----
+systemPrompt    <- "You are a helpful AI assistant"
+userPrompt      <- "What is the Apple stock ticker?"
+maxTokens       <- 800
+temperature     <- 0.7
+openRouterModel <- 'openai/gpt-4.1-nano'   
+# meta-llama/llama-3.2-1b-instruct
+# openai/gpt-4.1-nano
+# deepseek/deepseek-v4-flash
+# minimax/minimax-m3
+# z-ai/glm-5.2,
+# meta-llama/llama-3.2-1b-instruct
+# google/gemini-3.1-flash-lite
+# anthropic/claude-haiku-4.5
+# openai/gpt-4o-mini-search-preview
+# openai/gpt-5.6-luna
+
+# Models that generate validated JSON
+#   google/gemini-2.5-flash-lite   
+#   openai/gpt-4.1-nano   
+jsonOutput      <- T
+
+# ---- Do not change below ----
+systemContent <- if(jsonOutput==T){
+  paste(systemPrompt, "Respond only with valid JSON.")
+} else {
+  systemPrompt
+  }
+
+requestBody <- list(
+  model = openRouterModel,
+  messages = list(
+    list(role = "system", content = systemContent),
+    list(role = "user",   content = userPrompt)
+  ),
+  max_tokens = maxTokens,
+  temperature = temperature
+)
+if (jsonOutput == TRUE) {
+  requestBody$response_format <- list(type = "json_object")
+}
+response <- request("https://openrouter.ai/api/v1/chat/completions") |>
+  req_headers(Authorization = paste("Bearer", OPENROUTER_API_KEY)) |>
+  req_body_json(requestBody) |>
+  req_perform()
+
+llmResponse <- resp_body_json(response)$choices[[1]]$message$content
+
+if (jsonOutput == TRUE) {
+  cat(jsonlite::prettify(llmResponse))
+} else {
+  cat(llmResponse)
+}
+# End
